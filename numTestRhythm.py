103c9c0df49f19b18e395f730ca3ffa47a15f865
Nick Jackson <naj53@drexel.edu>
2018-01-31 22:33:22 -0500

    Improving the number generation simulation testing python algorithm thing

--- numTestRhythm.py
+++ numTestRhythm.py
@@ -5,6 +5,7 @@ def test(start):
 	vals = []
 	delta = round(time.time() * 1000) - start
 	#print delta
+	cycleLength = 0
 	xMod = 0
 	yMod = 0
 	zMod = 0
@@ -12,32 +13,57 @@ def test(start):
 	rolVMod = 0
 	pitPMod = 0
 	rolPMod = 0
-	degRatio = 90.0 / 5000.0
-	if delta > 10000:
-		if delta < 33000:
+	delay = 5000
+	pause = 2000
+	amplitude = 1
+	testLength = 20000
+	pauseMod = 0
+	tdelta = delta - delay - (testLength / 4)
+	degRatio = 0.0
+
+	if delta > delay:
+
+		if tdelta > 0:
+			pauseMod += 1
+			while tdelta > pause:
+				#print tdelta
+				tdelta -= pause 
+				tdelta -= (testLength / 4)
+				pauseMod += 1
+		if tdelta >= 0:
+			#print tdelta
+			#print tdelta % pause
+			print "Extending"
+			degRatio = (360.0 / (testLength + ((tdelta % pause) + (pause * pauseMod))))
+		else:
+			degRatio = (360.0 / (testLength + (pause * pauseMod)))
+
+		cycleLength = (delay + testLength + ((4 * pause) * (1 + int(pauseMod / 4))))
+
+		if delta < cycleLength:
 			xMod = 1
-		elif delta < 56000:
+		elif delta < cycleLength * 2:
 			yMod = 1
-		elif delta < 79000:
+		elif delta < cycleLength * 3:
 			zMod = 1
-		elif delta < 102000:
+		elif delta < cycleLength * 4:
 			pitVMod = 1
-		elif delta < 125000:
+		elif delta < cycleLength * 5:
 			rolVMod = 1
-		elif delta < 148000:
+		elif delta < cycleLength * 6:
 			pitPMod = 1
-		elif delta < 171000:
+		elif delta < cycleLength * 7:
 			rolPMod = 1
 
 	#print str(xMod) + " " + str(yMod) + " " + str(zMod)
 	#print degRatio
-	vals.append(xMod * math.sin(math.radians((delta - 10000) * degRatio)))
-	vals.append(yMod * math.sin(math.radians((delta - 33000) * degRatio)))
-	vals.append(zMod * math.sin(math.radians((delta - 79000) * degRatio)))
-	vals.append(pitVMod * math.sin(math.radians((delta - 102000) * degRatio)))
-	vals.append(rolVMod * math.sin(math.radians((delta - 125000) * degRatio)))
-	vals.append(pitPMod * math.sin(math.radians((delta - 148000) * degRatio)))
-	vals.append(rolPMod * math.sin(math.radians((delta - 171000) * degRatio)))
+	vals.append(xMod * math.sin(math.radians((delta - delay) * degRatio)))
+	vals.append(yMod * math.sin(math.radians((delta - cycleLength) * degRatio)))
+	vals.append(zMod * math.sin(math.radians((delta - (cycleLength * 2)) * degRatio)))
+	vals.append(pitVMod * math.sin(math.radians((delta - (cycleLength * 3)) * degRatio)))
+	vals.append(rolVMod * math.sin(math.radians((delta - (cycleLength * 4)) * degRatio)))
+	vals.append(pitPMod * math.sin(math.radians((delta - (cycleLength * 5)) * degRatio)))
+	vals.append(rolPMod * math.sin(math.radians((delta - (cycleLength * 6)) * degRatio)))
 
 	return vals
 
